240f663de5f87dc4c6a1178458f033b0004d6bd4
Chawan Saeed <chawan.saeed02@gmail.com>
2024-07-27 21:03:24 +0300

    Initial Commit

---
+++ MTerm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MTerm
+//
+//  Created by Chawan Saeed on 7/27/24.
+//  Copyright © 2024 Chawan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
